0206e8726f84b4d868b3f1db8e80e17e3e76663e
Douglas Vidal <vidaldl01@gmail.com>
2023-09-21 13:41:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zapMQ-consumer
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const amqp = require('amqplib/callback_api');
+const cors = require('cors');
+
+const AMQP_URL = 'amqp://localhost';
+const QUEUE_NAME = 'chat';
+const PORT = 9876;
+
+const receivedMessages = [];
+
+app.use(cors());
+
+function consumeQueue() {
+  amqp.connect(AMQP_URL, function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+
+      channel.assertQueue(QUEUE_NAME, {
+        durable: true,
+      });
+
+      console.log('Aguardando mensagens da fila...');
+
+      channel.consume(
+        QUEUE_NAME,
+        function (msg) {
+          if (!msg) {
+            return;
+          }
+
+          try {
+            const message = JSON.parse(msg.content.toString());
+            receivedMessages.push(message);
+            console.log('Mensagem recebida: ', message);
+            console.log('Array de mensagens: ', receivedMessages);
+            channel.ack(msg);
+          } catch (error) {
+            console.error('Erro ao processar mensagem:', error);
+          }
+        },
+        {
+          noAck: false,
+        }
+      );
+    });
+  });
+}
+
+// Call consumeQueue before starting the HTTP server
+consumeQueue();
+
+app.get('/mensagens', (req, res) => {
+  res.json(receivedMessages);
+});
+
+app.listen(PORT, () => {
+  console.log('Servidor está ouvindo na porta', PORT);
+});
